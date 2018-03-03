dbbfbd988512229f642ec4e3e6ba2f730a61e20c
Cs.Zhang <308415210@qq.com>
2018-03-04 02:40:27 +0800

    update

--- scripts/manhua_scawler.py
+++ scripts/manhua_scawler.py
@@ -8,14 +8,12 @@ import re
 from bs4 import BeautifulSoup
 import requests
 
-file_out = 'D:\\zcs-genex\\180303\\t.jpg'
-
-
-
+file_out_dir = 'E:\\manhua'
 
 home_page = 'http://manhua.fzdm.com/058/'
 rq = requests.get(url=home_page)
-r = BeautifulSoup(rq.content,'lxml')
+r = BeautifulSoup(rq.content,"html.parser") #python3.5 bs4
+#r = BeautifulSoup(rq.content,"lxml") #python3.6 bs4
 #print(str(r))
 
 regu1 = '<li class="pure-u-1-2 pure-u-lg-1-4">(.*?)</a></li>'
@@ -24,7 +22,22 @@ re1 = re.compile(regu1)
 re2 = re.compile(regu2)
 sites = re1.findall(str(r))
 
+def get_pic(url):
+    try:
+        rq1 = requests.get(url)
+    except:
+        return 'E1'
 
+    r1 = BeautifulSoup(rq1.content, "html.parser") #python3.5 bs4
+    #r1 = BeautifulSoup(rq1.content, "lxml")  # python3.6 bs4
+    if len(str(r1)) < 500:
+        return 'E'
+    s1 = re.compile('img.src="(.*?)"\\+mhurl').findall(str(r1))
+    s2 = re.compile('var mhurl = "(.*?)"').findall(str(r1))
+    page = re.compile('id="mhona">(.*?)</a><a href').findall(str(r1))[0]
+    s = s1[0] + s2[0]
+    # print(s)
+    return s
 
 for i in sites:
     a = re2.findall(i)
@@ -32,16 +45,37 @@ for i in sites:
         url_1 = a[0]
         name = a[1]
         print(url_1,name)
+        pic_dir = file_out_dir + '\\' + str(name)
+        if not os.path.exists(pic_dir):
+            os.mkdir(pic_dir)
         url_all = home_page + url_1
-        rq1 = requests.get(url_all)
-        r1 = BeautifulSoup(rq1.content,'lxml')
-        s1 = re.compile('img.src="(.*?)"\\+mhurl').findall(str(r1))
-        s2 = re.compile('var mhurl = "(.*?)"').findall(str(r1))
-        page = re.compile('id="mhona">(.*?)</a><a href').findall(str(r1))[0]
-        file_name = page + '.jpg'
-        s = s1[0] + s2[0]
+        for page in range(30):
+            url_c = url_all + 'index_' + str(page) + '.html'
+            #print(url_c)
+            if len(str(page)) == 1:
+                page = '0' + str(page)
+            file_name = str(page) + '.jpg'
+            if not os.path.exists(pic_dir + '\\' + file_name):
+                pic_url = get_pic(url_c)
+                if pic_url == 'E':
+                    break
+                elif pic_url == 'E1':
+                    print(str(page),'error')
+                else:
+                    file_name_o = open(pic_dir + '\\' + file_name, 'wb')
+                    try:
+                        rb = requests.get(pic_url)
+                        time.sleep(0.2)
+                        file_name_o.write(rb.content)
+                        file_name_o.close()
+                        size = os.path.getsize(pic_dir + '\\' + file_name)
+                        print(str(page),'downloaded',size)
+                        if not size:
+                            os.remove(pic_dir + '\\' + file_name)
+                            print(str(page),'removed')
+                    except:
+                        print(str(page),"can't downloading !!!!!")
+            else:
+                print(page,'pass')
 
-        print(s)
-    break
 
-    
